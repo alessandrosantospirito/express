26da498ea75138b7fb7ae5622b4e131fee43fbd5
Alessandro Santospirito <alessandro.santospirito@gmx.de>
2018-08-12 17:44:35 +0200

    finishing with simple arguments

---
+++ Middlewear/test.js
@@ -0,0 +1,27 @@
+const specific = function(req, res, next) {
+  console.log("Reacting to all connections regarding '/'");
+  next();
+};
+
+const alwaysListening = function(req, res, next) {
+  console.log("I'm always listening!");
+  next();
+};
+
+const requestTime = function(req, res, next) {
+  req.time = Date.now();
+  req.name = "Bert";
+  next();
+};
+
+const modifyName = function (req, res, next, name) {
+  let modified = name + "-dude";
+
+  req.name = modified;
+
+  next();
+}
+
+module.exports = {
+  specific
+};
--- server.js
+++ server.js
@@ -1,14 +1,78 @@
 let express = require("express");
 let app = express();
 let port = process.env.PORT || 8080;
+let mw = require("./Middlewear/test.js");
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Middlewear
+//-------------------------------
+app.all("/", mw.specific);
+
+app.use(mw.alwaysListening);
+
+app.use(mw.requestTime);
+
+app.param("name", mw.modifyName);
+
+//Get- working with regex
+//-------------------------------
 app.get("/", function(req, res) {
   res.send("root");
 });
 
-app.get("/about", function(req, res) {
-  res.send("about");
+//Getting the information from the middlewear "requestTime"
+app.get("/time", function(req, res) {
+  res.send(req.name + " askes at: " + req.time);
 });
 
+//Name form Http request with middlewear inbetween
+app.get("/catchingname/:name", function(req, res) {
+  res.send("Also I react to the name " + req.name);
+});
+
+//Same
+app.get("/api/users/:name", function(req, res) {
+  res.send("Whats is up " + req.name + "!");
+});
+
+//<?id=...&token=...&id=...>
+app.get("/api/users", function(req, res) {
+  let user_id = req.param("id");
+  let token = req.param("token");
+  let geo = req.param("geo");
+
+  res.send(user_id + " " + token + " " + geo);
+});
+
+//Using the http request for information
+//<?name=Nagobert&age=50> or simply postman
+app.get("/multyParameters", function(req, res) {
+  let user_name = req.param("name");
+  let user_age = req.param("age");
+
+  res.send(user_name + " is at the age of " + user_age);
+});
+
+//Post
+//-------------------------------
+app.post("/", function(req, res) {
+  res.send("Sucessfull");
+});
+
+//Use bodyParser to get body information
+app.post("/multyParameters", function(req, res) {
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+  let user_name = req.body.name;
+  let user_age = req.body.age;
+
+  res.send(user_name + " " + user_age);
+});
+
+//Starting port
 app.listen(port);
 console.log("Server started! At http://localhost:" + port);
