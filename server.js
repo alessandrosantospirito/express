c3fbebab471fe219c4d20409661d5291c50497b8
Alessandro Santospirito <alessandro.santospirito@gmx.de>
2018-08-12 12:40:42 +0200

    initalised express

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 let port = process.env.PORT || 8080;
 
+app.get("/", function(req, res) {
+  res.send("root");
+});
+
+app.get("/about", function(req, res) {
+  res.send("about");
+});
+
 app.listen(port);
-console.log.apply('Server started! At http://localhost: ' + port);
+console.log("Server started! At http://localhost:" + port);
